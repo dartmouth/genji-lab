59f0409e5e285d2e2ec824d2eb180385c92eb1ed
meehanti23 <meehanti23@gmail.com>
2025-03-19 16:24:51 -0400

    Comment card updated to show icons

--- core-ui/src/components/AnnotationCard.tsx
+++ core-ui/src/components/AnnotationCard.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Annotation } from "../types/annotation";
+import { FaThumbsUp, FaComment, FaFlag, FaCog } from "react-icons/fa";
+// import { useApiClient } from "../hooks/useApi";
+
 
 interface AnnotationCardProps {
     id: string;
@@ -9,6 +12,9 @@ interface AnnotationCardProps {
 
 
 const AnnotationCard: React.FC<AnnotationCardProps> = ({ id, annotation, isHighlighted = false }) => {
+    // const [settings, showSettings] = useState(false);
+    // let annotation_api = useApiClient();
+
     return (
         <div 
             key={id} 
@@ -22,9 +28,25 @@ const AnnotationCard: React.FC<AnnotationCardProps> = ({ id, annotation, isHighl
                 margin: '10px 0',
             }}
         >
-            <div className="comment-header">{`${annotation.creator.first_name} ${annotation.creator.last_name}`}</div>
+            <div className="comment-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <span>{`${annotation.creator.first_name} ${annotation.creator.last_name}`}</span>
+                <div>
+                    <button title="Upvote" style={{ background: 'none', border: 'none', cursor: 'pointer', color: 'green' }}>
+                        <FaThumbsUp />
+                    </button>
+                    <button title="Reply" style={{ background: 'none', border: 'none', cursor: 'pointer', color: 'blue' }}>
+                        <FaComment />
+                    </button>
+                    <button title="Flag" style={{ background: 'none', border: 'none', cursor: 'pointer', color: 'red' }}>
+                        <FaFlag />
+                    </button>
+                    <button title="Settings" style={{ background: 'none', border: 'none', cursor: 'pointer', color: 'black' }}>
+                        <FaCog />
+                    </button>
+                </div>
+            </div>
             <div className="comment-body">{annotation.body.value}</div>
-        </div>
+        </div>    
     )
 }
 
